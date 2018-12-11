d090ddd03db528a2f1fa0d91beb4b4768df3e907
Lisa Scott <lrscot@aol.com>
2018-12-11 01:11:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++){
+    var str = musicians[i] + " plays " + instruments[i];
+    arr.push(str);
+
+  }
+  return arr;
+}
+function johnLennonFacts(facts){
+    var arr = [];
+    var i = 0;
+    while (i < facts.length){
+      var str = facts[i] + "!!!";
+      arr.push(i);
+      i++;
+    }
+    return arr;
+}
